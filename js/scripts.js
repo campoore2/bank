18a92f0a6b38989aa252a0a20159fb26fbe273ad
Cameron Poore and Matthew Giem <cameron+matthew@>
2016-06-14 16:40:31 -0700

    Added some CS to allow for somewhat functionable scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
-$(document).ready(function(){
-  $("#acctCreate").click(function(){
-    event.preventDefault();
+function userDatabase(userName, totalBal) {
+  this.userName = userName;
+  this.totalBal = totalBal;
+}
+
+
+$(document).ready(function()  {
+  // $("#deposit").click(function(){
+  //   // alert("hi");
+  //   // var deposit = parseInt($("#deposit").val());
+  //   // var withdrawal = parseInt($("#withdrawal").val());
+  //   // var acctTotal = new userDatabase(userName, totalBal);
+  //   // event.preventDefault();
+  // });
+  $("#acctFunc").hide();
+  $("#acctCreate").submit(function(event){
     var userName = $("#userName").val();
     var totalBal = parseInt($("#inDepositAmt").val());
     var user = new userDatabase(userName, totalBal);
-  });
-});
+    debugger;
+    $("#acctFunc").show();
+    $("#signUp").hide();
+    $("#total").text(user.totalBal);
 
-function userDatabase(userName, totalBal){
-  this.userName = userName;
-  this.totalBal = totalBal;
+    event.preventDefault();
+  });
+})
+
+userDatabase.prototype.deposit = function(deposit){
+  return this.totalBal + deposit;
 }
